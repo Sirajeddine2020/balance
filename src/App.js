c2ae60c4bf7d1b0f9fa067c64521322108eaef91
Sirajeddine2020 <sirajbouasker@gmail.com>
2021-05-27 21:09:00 +0200

    second balance commit added a custom hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 const App = () => {
+ 
   const [balance, setBalance] = useState(1500);
   const [savingBalance, setSavingBalance] = useState(1349);
   const [notification, setNotification] = useState(false);
+
+  function useCustom(x,y) {
+    const [name,setName]=useState('Meyssa');
+    const [phone,setPhone]=useState(55454512120);
+       useEffect(() => {
+        setName(x);
+        setPhone(y);
+        
+    }, [x,y]);
+  return {'name':name, 'phone':phone};
+  }
+ 
+
 
   const handleSavings = () => {
     if (balance > 1000) {
@@ -17,6 +31,12 @@ const App = () => {
     setSavingBalance(savingBalance - 100);
   }
 
+
+ let obj= useCustom('Sirine', 227841253225);
+ let obj2= useCustom('Aida', 22744441253225);
+ let obj3= useCustom('Rania', 288888888885);
+
+
   useEffect(() => {
     if (balance <= 1000) {
       setNotification(true);
@@ -27,6 +47,12 @@ const App = () => {
     <div className="columns">
       <div className="column">
         <div className="box">
+         <h2>my name is {obj.name}</h2>
+         <h3>my phone is {obj.phone}</h3>
+         <h2>my name is {obj2.name}</h2>
+         <h3>my phone is {obj2.phone}</h3>
+         <h2>my name is {obj3.name}</h2>
+         <h3>my phone is {obj3.phone}</h3>
           <h4 className="title is-4">Your account balance:</h4>
           <div className="amount">{balance}$</div>
           <button
